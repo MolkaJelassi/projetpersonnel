e57d898b1351d3e712bf89863b27693b7f3c291a
MolkaJelassi <molka.jelassi@esprit.tn>
2017-03-08 22:04:04 +0100

    e

---
+++ projet-personnel/client/src/main/java/test/TestDeleteTraining.java
@@ -0,0 +1,20 @@
+package test;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import tn.esprit.beautifulminds.persistence.Training;
+import tn.esprit.beautifulminds.services.crud.TrainingservicesRemote;
+
+public class TestDeleteTraining {
+	public static void main(String[] args) throws NamingException {
+		Context context = new InitialContext();
+		TrainingservicesRemote trainingservicesRemote = (TrainingservicesRemote) context.lookup(
+				"projet-personnel-ear/projet-personnel-ejb/Trainingservices!tn.esprit.beautifulminds.services.crud.TrainingservicesRemote");
+
+		Training training1 = trainingservicesRemote.findTrainingById(1);
+
+		trainingservicesRemote.deleteTraining(training1);
+	}
+}
